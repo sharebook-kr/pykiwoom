fec86add24e1f0ad61591da943ef66790de88e18
Brayden Jo <brayden.jo@outlook.com>
2020-06-24 08:11:50 +0900

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name            = 'pykiwoom',
-    version         = '0.0.4',
+    version         = '0.0.5',
     description     = 'python wrapper for Kiwoom Open API+ (Korea Stock Market)',
     url             = 'https://github.com/sharebook-kr/pykiwoom',
     author          = 'Lukas Yoo, Brayden Jo',
