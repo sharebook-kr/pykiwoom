a6055f890374752e6e7113174332bdd9ee6a9dbf
Kyungwon Chun <ruddyscent@gmail.com>
2021-10-03 14:08:57 +0900

    Add dependency on pypiwin32
    The import statement reports an error related to 'pythoncom.'
    ```
    >>> from pykiwoom.kiwoom import *
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "C:\Anaconda3\envs\kiwoom\lib\site-packages\pykiwoom\kiwoom.py", line 4, in <module>
        import pythoncom
    ModuleNotFoundError: No module named 'pythoncom'
    ```
    This package seems to depend on 'pypiwin32.'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url             = 'https://github.com/sharebook-kr/pykiwoom',
     author          = 'Lukas Yoo, Brayden Jo',
     author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com, pystock@outlook.com',
-    install_requires= ['pandas'],
+    install_requires= ['pandas', 'pypiwin32'],
     license         = 'MIT',
     packages        = ['pykiwoom'],
     zip_safe        = False
