a6e7db9993d238e6e66e7016899257a7c2eda8ac
Brayden <brayden.jo@outlook.com>
2021-12-25 15:41:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name            = 'pykiwoom',
-    version         = '0.0.6',
+    version         = '0.0.7',
     description     = 'python wrapper for Kiwoom Open API+ (Korea Stock Market)',
     url             = 'https://github.com/sharebook-kr/pykiwoom',
     author          = 'Lukas Yoo, Brayden Jo',
     author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com, pystock@outlook.com',
-    install_requires= ['pandas'],
+    install_requires= ['pandas', 'pyqt5', 'pywin32'],
     license         = 'MIT',
     packages        = ['pykiwoom'],
     zip_safe        = False
