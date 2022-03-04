48210a5547a3ff0217e69350b475139370ee9ec8
Brayden <brayden.jo@outlook.com>
2022-03-04 17:33:45 +0900

    update email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description     = 'python wrapper for Kiwoom Open API+ (Korea Stock Market)',
     url             = 'https://github.com/sharebook-kr/pykiwoom',
     author          = 'Lukas Yoo, Brayden Jo',
-    author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com, pystock@outlook.com',
+    author_email    = 'brayden.jo@pyquant.co.kr, jonghun.yoo@pyquant.co.kr',
     install_requires= ['pandas', 'pyqt5', 'pywin32'],
     license         = 'MIT',
     packages        = ['pykiwoom'],
